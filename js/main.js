a8c1dd356e8f32533a3fc14223f04ccff1b2c76f
Natalia Bashirova <tac-2014@yandex.ru>
2024-03-11 17:47:22 +0100

    Выносит логику отрисовки случайных фото в контейнере в main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-// import {photosData} from './data.js';
-// import {MAX_PHOTOS} from './const.js';
-import './thumbnails.js';
+import {photosData} from './data.js';
+import {MAX_PHOTOS} from './const.js';
+import {createThumbnail} from './thumbnails.js';
+import {renderPack} from './utils.js';
 
+// Генерит случайные фотографии
+const photos = photosData(MAX_PHOTOS);
 
+// Находит контейнер для фото и выводит все фото на экран
+const photosBox = document.querySelector('.pictures');
+renderPack (photos,createThumbnail,photosBox);
