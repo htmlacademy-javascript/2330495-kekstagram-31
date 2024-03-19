8a57575b128096e63fcbfdf05c1f72f05fafcdab
Natalia Bashirova <tac-2014@yandex.ru>
2024-03-19 17:25:21 +0100

    Переименовывает параметры функции создания миниатюр

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,14 +1,14 @@
 import {findTemplate} from './utils.js';
 
-const createThumbnail = (photo)=>{
+const createThumbnail = ({url,description,comments,likes})=>{
   const thumbnail = findTemplate('#picture').cloneNode(true);
   const image = thumbnail.querySelector('.picture__img');
 
-  image.src = photo.url;
-  image.alt = photo.description;
+  image.src = url;
+  image.alt = description;
 
-  thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
-  thumbnail.querySelector('.picture__likes').textContent = photo.likes;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
 
   return thumbnail;
 };
