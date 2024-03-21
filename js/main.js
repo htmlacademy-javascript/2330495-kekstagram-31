ab7f3c1f556fd455caae33f196d361fd30a742fb
Natalia Bashirova <tac-2014@yandex.ru>
2024-03-21 18:32:12 +0100

    Добавляет обработчик открытия большого окна

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-import {photosData} from './data.js';
-import {MAX_PHOTOS} from './const.js';
+import {picturesData} from './data.js';
 import {createThumbnail} from './thumbnails.js';
 import {renderListNode} from './utils.js';
-import {photosBox} from './open-close-bigphoto';
+import {openBigPhoto} from './photo handlers.js';
 
-// Генерит случайные фотографии
-const photos = photosData(MAX_PHOTOS);
+const thumbnailsBox = document.querySelector('.pictures');
 
-// Выводит все фото случайного пользователя на экран
-renderListNode({dataItems:photos, createdNote:createThumbnail,container:photosBox});
+// Выводит все фото случайных пользователей на экран с помощью шаблона
+renderListNode({dataItems:picturesData, createdNote:createThumbnail, container:thumbnailsBox});
 
+// Обработчик который открывает большую фотографию
+thumbnailsBox.addEventListener('click', openBigPhoto);
