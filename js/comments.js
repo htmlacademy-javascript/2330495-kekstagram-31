3cb3f07bc0b0661424a902ee6cfb510b6a038251
Natalia Bashirova <tac-2014@yandex.ru>
2024-03-21 18:33:07 +0100

    Заполняет темплейт по комментам данными

---
+++ js/comments.js
@@ -0,0 +1,15 @@
+import {findTemplate} from './utils.js';
+
+const createComments = ({avatar, message, user})=>{
+  const socialComment = findTemplate('#comment').cloneNode(true);
+  const avatarImage = socialComment.querySelector('.social__picture');
+
+  avatarImage.src = avatar;
+  avatarImage.alt = user;
+
+  socialComment.querySelector('.social__text').textContent = message;
+
+  return socialComment;
+};
+
+export {createComments};
