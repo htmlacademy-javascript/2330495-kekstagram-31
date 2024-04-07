05880756a5cbb19b3dacc29a9908ea2d662d067b
Natalia Bashirova <tac-2014@yandex.ru>
2024-04-07 19:56:49 +0200

    Выносит в константы переменные

--- js/const.js
+++ js/const.js
@@ -1,6 +1,11 @@
 
 const SCALE_STEP = 0.25;
 const COMMENTS_STEP = 5;
-const REMOVE_MESSAGE_TIMEOUT = 5000 ;
+const REMOVE_MESSAGE_TIMEOUT = 5000;
 
-export{COMMENTS_STEP , SCALE_STEP, REMOVE_MESSAGE_TIMEOUT};
+const imgUploadForm = document.querySelector('.img-upload__form');
+const imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
+const pageBody = document.querySelector('body');
+const bigPhotoContainer = document.querySelector('.big-picture');
+
+export{COMMENTS_STEP , SCALE_STEP, REMOVE_MESSAGE_TIMEOUT ,imgUploadForm, imgUploadOverlay, pageBody, bigPhotoContainer};
