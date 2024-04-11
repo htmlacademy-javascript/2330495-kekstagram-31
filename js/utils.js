c6f943e4167c81502946b6a3e610df77964ed16f
Natalia Bashirova <tac-2014@yandex.ru>
2024-04-11 20:44:49 +0200

    Исправляет модуль

--- js/utils.js
+++ js/utils.js
@@ -1,26 +1,25 @@
 import {DEBOUNCE_DELAY} from './const';
 
-// Находит template в разметке
+
 const findTemplate = (id) =>{
   const template = document.querySelector(id);
   if(!template){
     return ('Template not found');
   }
-  // Проверка на тип узла DOM
+
   if (!(template instanceof HTMLTemplateElement)) {
     return ('Element is not a template');
   }
   return template.content.firstElementChild;
 };
 
-// Создает функцию добавления фрагмента в контейнер
+
 const renderListNode = ({dataItems, createdNote, container}) =>{
   const fragment = document.createDocumentFragment();
   dataItems.forEach((item) => fragment.append(createdNote(item)));
   container.append(fragment);
 };
 
-// Cоздает функцию проверки нажатой клавиши
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const debounce = (callback, timeoutDelay = DEBOUNCE_DELAY) => {
@@ -32,7 +31,6 @@ const debounce = (callback, timeoutDelay = DEBOUNCE_DELAY) => {
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
 };
-
 
 export { findTemplate, renderListNode, isEscapeKey,debounce};
 
